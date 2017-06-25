9fc3ea373a47c56c336d5e9c52056ab2a5d32f63
wolfgangwittner <wolfgang.wittner@trivadis.com>
2017-06-25 23:46:09 +0200

    Add files via upload
    Added Introduction

---
+++ 01_Introduction/Solution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using upgradetoweb;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		//string name;
+            	//Console.WriteLine("Hello World!");
+		//name = Console.ReadLine();
+		//Console.WriteLine($"Hello {name}");	
+
+		var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.Build();
+		host.Run();
+ 		
+        }
+    }
+}
---
+++ 01_Introduction/Solution/Startup.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace upgradetoweb
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			app.Run(context =>  {
+				return context.Response.WriteAsync("Hello Someone in the Web!");
+			});
+		}
+	}
+
+}
